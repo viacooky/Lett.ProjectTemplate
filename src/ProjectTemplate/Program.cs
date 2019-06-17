8c7af212c0a777e9361ca7f82ecdd1120b29a5b5
viacooky <viacooky@qq.com>
2019-06-17 13:09:02 +0800

    init

---
+++ src/ProjectTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
